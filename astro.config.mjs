11280380aff7ddd57526bbe9ab20fb307a5487e4
barry <hypier@qq.com>
2024-10-03 11:20:38 +0800

    禁用Cloudflare适配器的PlatformProxy设置
    已将Cloudflare适配器的配置更改回默认设置，移除了对platformProxy.enabled的显式定义。这一变更逆转了先前启用PlatformProxy的决定，恢复了配置文件至不含该选项的状态。

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,11 +34,12 @@ export default defineConfig({
   // experimental: {
   //   viewTransitions: true
   // },
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+  adapter: cloudflare(),
+  // adapter: cloudflare({
+  //   platformProxy: {
+  //     enabled: true
+  //   }
+  // }),
   // adapter: node({ mode: 'standalone' }),
   image: {
     service: squooshImageService(),
