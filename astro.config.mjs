9855fbe60b5b022b49953346796d5686f5bcf505
barry <hypier@qq.com>
2024-11-04 21:22:37 +0800

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -55,7 +55,7 @@ export default defineConfig({
     sitemap({
       priority: 0.7,
       changefreq: 'weekly',
-      lastmod: new Date().toISOString(),
+      lastmod: new Date('2024-11-04'),
     }),
     tailwind({
       applyBaseStyles: false,
