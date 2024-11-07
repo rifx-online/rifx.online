adae7e9e5c148b7efb3d7ae018b38daee27b5c36
barry <hypier@qq.com>
2024-11-07 17:57:38 +0800

    build: 更新站点地图最后修改日期
    - 将 sitemap 配置中的 lastmod 字段从 2024-11-04 修改为 2024-11-07

--- astro.config.mjs
+++ astro.config.mjs
@@ -55,7 +55,7 @@ export default defineConfig({
     sitemap({
       priority: 0.7,
       changefreq: 'weekly',
-      lastmod: new Date('2024-11-04'),
+      lastmod: new Date('2024-11-07'),
     }),
     tailwind({
       applyBaseStyles: false,
