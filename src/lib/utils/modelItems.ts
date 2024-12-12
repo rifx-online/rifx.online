afc7fb9833cf7ccef34bd6d68d581f51b7cf6396
barry <hypier@qq.com>
2024-12-12 08:30:37 +0800

    sort

--- src/lib/utils/modelItems.ts
+++ src/lib/utils/modelItems.ts
@@ -10,13 +10,17 @@ const recommendedItems = (allPosts: any[]) => {
 
 // 免费模型
 export const freeItems = (allPosts: any[]) => {
-  const freePosts = allPosts.filter((post) => post.data.is_free && post.data.is_active);
+  const freePosts = allPosts
+    .filter((post) => post.data.is_free && post.data.is_active)
+    .sort((a, b) => new Date(b.data.last_updated).getTime() - new Date(a.data.last_updated).getTime());
   return freePosts;
 };
 
 // 打折模型
 export const discountItems = (allPosts: any[]) => {
-  const discountPosts = allPosts.filter((post) => post.data.discount < 1 && post.data.is_active);
+  const discountPosts = allPosts
+    .filter((post) => post.data.discount < 1 && post.data.is_active)
+    .sort((a, b) => new Date(b.data.last_updated).getTime() - new Date(a.data.last_updated).getTime());
   return discountPosts;
 };
 
